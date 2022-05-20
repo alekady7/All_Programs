7bf60b064b853ac6bdbd2fa88fc6cf72478cd413
Aditya Uniyal <uniyal2001@gmail.com>
2022-05-20 22:57:32 +0530

    This is my first commit for the leap year program

---
+++ leap_year.java
@@ -0,0 +1,14 @@
+public class leap_year{
+    public static void main(String[] args) {
+        int year = 2016;
+        if(year % 4 == 0){
+            if (year % 400 == 0 && year % 100 != 0){
+                System.out.println(year + " is a leap year");
+            } else{
+                System.out.println(year + " is not a leap year");
+            }
+        }else {
+            System.out.println(year + " is not a leap year");
+        }
+    }
+}
