657e81ccb74ab0ee62d9c3481db7119397c4c0ae
Aditya Uniyal <uniyal2001@gmail.com>
2022-05-20 23:05:27 +0530

    This is my first commit for the leap year program

--- leap_year.java
+++ leap_year.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class leap_year{
     public static void main(String[] args) {
-        int year = 2016;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a year!!!");
+        int year = sc.nextInt();
         if(year % 4 == 0){
             if (year % 400 == 0 && year % 100 != 0){
                 System.out.println(year + " is a leap year");
