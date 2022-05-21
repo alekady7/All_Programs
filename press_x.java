41ede9a259f09a7ee9dee045935a12f585d3430a
Aditya Uniyal <uniyal2001@gmail.com>
2022-05-22 00:45:07 +0530

    Infinite sum until user press a certain number

---
+++ press_x.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class press_x {
+    public static void main(String[] args) {
+        char a = 'x';
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a no: ");
+        int n=0,sum=0;
+        int num = 5;
+        while(n != num){
+            n = sc.nextInt();
+            sum += n;
+        }
+        System.out.println("program ended");
+        System.out.println("The sum is: " + sum);
+    }
+}
