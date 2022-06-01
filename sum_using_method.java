cceed788823903c489b5f88317e9845f81ffe9c3
Aditya Uniyal <uniyal2001@gmail.com>
2022-06-01 20:01:01 +0530

    calc using method

--- sum_using_method.java
+++ sum_using_method.java
@@ -2,8 +2,23 @@ import java.util.Scanner;
 
 public class sum_using_method {
     public static int sum(int num1, int num2){
-        int sums = num1 + num2;
-        return sums;
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    public static int mul(int num1, int num2){
+        int mul = num1 * num2;
+        return mul;
+    }
+
+    public static int sub(int num1, int num2){
+        int sub = num1 - num2;
+        return sub;
+    }
+
+    public static int div(int num1, int num2){
+        int div = num1 / num2;
+        return div;
     }
 
     public static void main(String[] args) {
@@ -12,6 +27,9 @@ public class sum_using_method {
         int num1 = sc.nextInt();
         System.out.print("Enter another no: ");
         int num2 = sc.nextInt();
-        System.out.print("Sum is : " + sum(num1, num2));
+        System.out.print("Sum is : " + sum(num1, num2)+"\n");
+        System.out.print("mul is : " + mul(num1, num2)+"\n");
+        System.out.print("Sub is : " + sub(num1, num2)+"\n");
+        System.out.print("div is : " + div(num1, num2));
     }
 }
