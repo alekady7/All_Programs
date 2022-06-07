448f62eabed46b1c50e598b44a51dcfdffda1922
Aditya Uniyal <uniyal2001@gmail.com>
2022-06-07 14:34:07 +0530

    3D array

---
+++ array_3d.java
@@ -0,0 +1,13 @@
+public class array_3d {
+    public static void main(String[] args){
+        int[][][] arr = {{{1, 2},{3, 4}},{{5, 6},{7, 8}}};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                for(int k=0;k<arr.length;k++){
+                    System.out.print(arr[i][j][k]);
+                }
+                System.out.println();
+            }
+        }
+    }
+}
