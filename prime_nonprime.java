3c56e83e9e9839c77d134f3b42cf9a95a2224188
Aditya Uniyal <uniyal2001@gmail.com>
2022-06-18 12:15:27 +0530

    prime number

--- prime_nonprime.java
+++ prime_nonprime.java
@@ -1,29 +1,31 @@
 import java.util.Scanner;
 
 public class prime_nonprime {
-    public static boolean prime(int num){
-        if(num<1){
-            return false;
-        }
-        if(num<=3){
-            return true;
-        }
-        if(num%2 == 0 || num%3==0){
-            return false;
-        }
-        for(int i=5;i*i <= num;i = i + 6){
-            if(num%i == 0 || num%(i+2) == 0){
+    //efficient method
+    public static boolean isPrime(int n){
+        if(n==1) return false;
+        for(int i=2;i*i<=n;i++)
+            if(n%i == 0)
                 return false;
-            }
-        }
+        return true;
+    }
+
+    //More efficient method
+    public static boolean isPrimeOrNot(int n){
+        if(n == 1) return false;
+        if(n == 2 || n == 3) return true;
+        if(n%2 == 0 || n%3 == 0) return false;
+        for(int i=5;i*i<=n;i+=6)
+            if(n%i == 0 || n%(i+2) == 0)
+                return false;
         return true;
     }
 
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("PRIME OR NOT!!");
-        System.out.println("Enter a no: ");
-        int num = sc.nextInt();
-        System.out.println(num +": " + prime(num));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a no: ");
+        int n=sc.nextInt();
+        //System.out.print(isPrime(n));
+        System.out.print(isPrimeOrNot(n));
     }
 }
